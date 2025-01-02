fad6b13f58a7f633a40d8a92b214840b29add2d7
Henok Wehibe <henokwehibe@gmail.com>
2025-01-02 00:52:19 +0000

    adding exex

--- flaskr/api.py
+++ flaskr/api.py
@@ -1,5 +1,6 @@
+import subprocess
 from flask import (
-    Response, Blueprint, request, redirect, send_file,  url_for,  render_template, 
+    Response, Blueprint, json, request, redirect, send_file,  url_for,  render_template, 
 )
 from flask import flash
 import os
@@ -17,30 +18,28 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+messages = []
 
 
 
-@bp.route('/uploads', methods=['GET', 'POST'])
-def upload_file():
+
+
+
+@bp.route('/', methods=('GET', 'POST'))
+def create():
     if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename) # type: ignore
-            file.save(os.path.join( 'flaskr/static/uploads',filename))
-            alpr_file = (os.path.join('flaskr/static/uploads', filename))
-            return redirect(url_for('api.upload_file'))
-    return '''
-    <!doctype html>
-    <title>Upload</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+        first_name = request.form['first-name']
+        last_name = request.form['last-name']
+        print(first_name)
+        if not first_name:
+            flash('Name is required!')
+        elif not last_name:
+            flash('Last is required!')
+        else:
+            messages.append({
+                'first-name': first_name,
+                  'last-name': last_name,
+                    })
+   
+            return redirect(url_for('api.create'))
+    return render_template('proc/index.html', messages=messages)
