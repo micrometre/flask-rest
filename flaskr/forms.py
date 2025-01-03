a411f6615fe16bcdc5898799671247b8f54eff0a
Henok Wehibe <henokwehibe@gmail.com>
2025-01-03 01:21:37 +0000

    added form

--- flaskr/forms.py
+++ flaskr/forms.py
@@ -107,9 +107,9 @@ def update(id):
                 "UPDATE post SET title = ?, body = ? WHERE id = ?", (title, body, id)
             )
             db.commit()
-            return redirect(url_for("blog.index"))
+            return redirect(url_for("forms.index"))
 
-    return render_template("blog/update.html", post=post)
+    return render_template("forms/update.html", post=post)
 
 
 @bp.route("/<int:id>/delete", methods=("POST",))
@@ -122,6 +122,6 @@ def delete(id):
     """
     get_post(id)
     db = get_db()
-    db.execute("DELETE FROM post WHERE id = ?", (id,))
+    db.execute("DELETE FROM forms WHERE id = ?", (id,))
     db.commit()
-    return redirect(url_for("blog.index"))
+    return redirect(url_for("forms.index"))
